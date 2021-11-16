849e0cc61e9ae0bb33467c9c36efcfacc245b66d
KolheSachinA <sachinkolhe719@gamil.com>
2021-11-16 21:36:46 +0530

    On16-11

--- myProject/routes/addRecord.js
+++ myProject/routes/addRecord.js
@@ -4,8 +4,9 @@ const mongoConnection = require('../utils/connection').connectionObject;
 // Import that connection object
 const jwt = require('jsonwebtoken');
 const importAuthFun = require('../utils/verifyAuth');
+
 /* GET home page. */
-router.get('/addRecord', function(req, res, next) {
+router.get('/addRecord', function (req, res, next) {
   // Database Name
   importAuthFun.verifyJwtAuth(req.cookies.jwtToken.code).then((result) => {
     if (result === 'Verified Successfully') {
@@ -19,39 +20,38 @@ router.get('/addRecord', function(req, res, next) {
           let pageNo;
           let limitValue;
           // console.log(req.query.page);
-          if (req.query.page === undefined){
+          if (req.query.page === undefined) {
             pageNo = 1
             limitValue = 5;
           } else {
             pageNo = req.query.page;
             limitValue = req.query.limit;
           }
-          
-          //const insertResult = await collection.insertMany([{ a: 1, name:'Ram', city:'Bangalore'}]);
-          const findResult = await collection.find({}).limit(limitValue *1).skip((pageNo-1) *limitValue).toArray();
+          const findResult = await collection.find({}).limit(limitValue * 1).skip((pageNo - 1) * limitValue).toArray();
+          console.log(findResult.length);
           // console.log('Found documents =>', findResult);
           //  res.send({page,
           //           limit,data:findResult});
           const details = {
-            findResult,pageNo,limitValue
+            findResult, pageNo, limitValue
           }
           res.render('addRecord', { title: 'Express', details });
-        } catch(err) {
+        } catch (err) {
           console.log(err);
           res.status(500).send(err);
         }
-        
+
       });
-       // res.send('Token verified succesfully redirected to /testRoute page');
+      // res.send('Token verified succesfully redirected to /testRoute page');
     } else {
-        res.status(401).send('Incorrect Token or missing token');
+      res.status(401).send('Incorrect Token or missing token');
     }
-}).catch((err) => {
+  }).catch((err) => {
     res.status(401).send('Incorrect Token or missing token');
-})
+  })
 });
 
-  // res.render('index', { title: 'Express',arr : [1,2,3,4,5], obj :{a:'Ram',b:2,c:3} });
+// res.render('index', { title: 'Express',arr : [1,2,3,4,5], obj :{a:'Ram',b:2,c:3} });
 
 
 module.exports = router;
